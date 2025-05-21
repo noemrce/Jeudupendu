8e7ea494024eb2732be492669977343f55d4cd93
Noé <noe.morance.1@ens.etsmtl.ca>
2025-05-21 10:36:11 -0400

    Création du fichier python pendu.py test de la fonction supprimer_accents test de la fonction charger_mots_pendu import de random implementation de la fonction jouer

--- pendu.py
+++ pendu.py
@@ -1,5 +1,3 @@
-print("welcome")
-
 def supprimer_accents(mot): #Fonction pour retirer les accents
     mot_sans_accents_1 = "" #on va ici stocker le mot modifié
     remplacement = {
@@ -30,3 +28,42 @@ import random
 
 def choisir_mot(mots):
     return random.choice(mots)
+
+def jouer():
+    mot_secret = choisir_mot(charger_mots_pendu()) # mot aleatoire sans accents
+    lettres_trouvees = [] # bonnes lettres trouvees
+    chances = 6 # nombres d'essais autorises
+
+    while chances > 0:
+        affichage = "" # va nous permettre d'afficher l'etat du mot
+
+        for lettre in mot_secret:
+            if lettre in lettres_trouvees:
+                affichage += lettre  # Lettre devinee : on l'affiche
+            else:
+                affichage += "_" # lettre pas devinee : on affiche _
+        print("Mot à deviner", affichage) #on a replacer la lettre trouvée au bonne endroit
+
+        if "_" not in affichage:
+            print ("félicitation, mot trouvé !")
+            return
+
+        choix_de_lettre = input("choisissez une lettre : ").lower() # entree du choix du joueur s'assurer d'avoir une minuscule
+
+        if choix_de_lettre in lettres_trouvees:
+            print("lettre déjà trouvée")
+
+        if choix_de_lettre in mot_secret :
+            print ("bonne réponse")
+            lettres_trouvees.append(choix_de_lettre) #
+
+        else:
+            print ("mauvaise réponse")
+            chances -= 1
+            print(f"il reste {chances} chances.")
+
+    print ("vous avez perdu. Le mot à deviner était :", mot_secret)
+
+jouer()
+
+input("Appuyez sur Entrée pour quitter")#Permet de run depuis mon folder

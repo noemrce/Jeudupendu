8cd956feae0018027d62cab983261c4075fa0f94
Noé <noe.morance.1@ens.etsmtl.ca>
2025-05-21 00:17:46 -0400

    Création du fichier python pendu.py test de la fonction supprimer_accents

--- pendu.py
+++ pendu.py
@@ -1,1 +1,20 @@
 print("welcome")
+
+def suprrimer_accents(mot): #Fonction pour retirer les accents
+    mot_sans = "" #on va ici stocker le mot modifié
+    remplacement = {
+        'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e',
+        'à': 'a', 'â': 'a', 'ä': 'a',
+        'î': 'i', 'ï': 'i',
+        'ô': 'o', 'ö': 'o',
+        'ù': 'u', 'û': 'u', 'ü': 'u',
+        'ç': 'c'
+      } #on simplifie chaque accent
+    for lettre in mot: #on parcourt chaque lettre du mot choisi
+        if lettre in remplacement:
+            mot_sans += remplacement[lettre] #on remplace la lettre accentué
+        else:
+            mot_sans += lettre
+    return mot_sans #on retourne le même mot sans accents
+
+print(suprrimer_accents("éàîôùç")) #test de la fonction
